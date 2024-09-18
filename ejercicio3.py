b713d5084c40336cb2a9a035cea2becc396dcce7
nisepesa <nisepesa1011@gmail.com>
2024-09-18 13:40:51 -0500

    done

--- ejercicio3.py
+++ ejercicio3.py
@@ -7,3 +7,18 @@
 # Más de 60000€              | 45%
 # Escribir un programa que pregunte al usuario su renta anual y muestre por pantalla el tipo impositivo que le corresponde.
 # (10p) (Nicolás +Lorena)
+
+rent=float(input('Escribir su renta anual: '))
+
+if rent < 10000:
+    tipo_impositivo = '5%'
+elif 10000 <= rent <= 20000:
+    tipo_impositivo = '15%'
+elif 20000 <= rent <= 35000:
+    tipo_impositivo = '20%'
+elif 35000 <= rent <= 60000:
+    tipo_impositivo = '30%'
+else:
+    tipo_impositivo = '45%'
+    
+print(f'Para su renta anual de {rent} debera pagar el {tipo_impositivo} de impositivo')

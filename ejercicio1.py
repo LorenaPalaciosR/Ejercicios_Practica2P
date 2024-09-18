4281925f6b53ef7b8ea86fb6dd150a733528aa54
JosuMoi270706 <moiseshdz7127@gmail.com>
2024-09-17 21:09:49 -0600

    Realiza ejercicio 1

--- ejercicio1.py
+++ ejercicio1.py
@@ -2,3 +2,9 @@
 # pregunte al usuario por la contraseña e imprima por pantalla si la contraseña introducida
 # por el usuario coincide con la guardada en la variable sin tener en cuenta mayúsculas y minúsculas.
 # (10 p) (Josué - Antony(Carlos))
+contraseña = ("contraseña")
+contraseña_usuario = input("Introduce la contraseña: ")
+if contraseña.lower() == contraseña_usuario.lower():
+   print("La contraseña es correcta")
+else:
+    print("La contraseña es incorrecta") 

f049b0ae76e4aad8f5fe9046793b7a5b259bae88
OmarGuillenSan <bruno_huarcaya2@hotmail.com>
2024-09-18 21:06:08 -0400

    Realizar ejercicio 4

--- ejercicio4.py
+++ ejercicio4.py
@@ -2,3 +2,10 @@
 # el precio que debe cobrar a sus clientes por entrar. El programa debe preguntar al usuario la edad del 
 # cliente y mostrar el precio de la entrada. Si el cliente es menor de 4 años puede entrar gratis, 
 # si tiene entre 4 y 18 años debe pagar 5€ y si es mayor de 18 años, 10€. (10p) (Omar) 
+edad=int(input("Ingresar su edad : "))
+if(edad<4):
+    print(f"Su edad de {edad} le permite ingresar GRATISSSS!!")
+elif(edad>=4 and edad<=18):
+    print(f"Su edad de {edad} :Debera pagar 5 euros")
+else:
+    print(f"Su edad de {edad} :Debera pagar 10 euros")
